83b1cf36ab1343445d00923305088b318097708a
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-11-22 20:50:56 +0600

    update

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,7 @@
 import Main from "../../Layout/Main";
+import Test from "../../Layout/Test";
 import Login from "../../Login/Login";
+import Appointment from "../../Pages/Appointment/Appointment/Appointment";
 import Home from "../../Pages/Home/Home";
 
 const { createBrowserRouter } = require("react-router-dom");
@@ -11,5 +13,7 @@ export const router = createBrowserRouter([
         children: [
             {path: '/', element: <Home></Home>},
             {path: '/login', element: <Login></Login>},
+            { path: '/appointment', element: <Appointment></Appointment>},
+            
     ]}
 ])
